85e0edc8877282d787207405f033daba70944fba
flynnGodslayer <victor_avila_felipe@hotmail.com>
2018-05-22 13:44:16 -0500

    Update peticion_ajax.php

--- peticion_ajax.php
+++ peticion_ajax.php
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
 	$("button").click(function(){
 
-		$.get("consulta_bd.php", function(data, status){
+		$.get("consulta_bd.js", function(data, status){
 
 			$("#usuarios").html(data);
 
