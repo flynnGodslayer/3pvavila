c5d83342ceb9bed91d2444b4dcd0c55132f4b1fb
flynnGodslayer <victor_avila_felipe@hotmail.com>
2018-05-22 13:46:12 -0500

    Create consulta.php

---
+++ consulta.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset=“utf-8”>
+<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+<script src="js/jquery-3.2.1.min.js"></script>
+
+<script src="peticion_ajax.js"></script>
+
+</head>
+
+<body>
+
+<button class="btn btn-primary">Mostrar usuarios</button>
+
+<div id="usuarios">
+</div>
+
+</body>
+
+</html>
